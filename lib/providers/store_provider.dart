5b234dc453be3723b90944bb937de2f1b2e17042
Saad Nauman <saadijutt007@gmail.com>
2023-06-11 20:05:41 +0500

    Added partial store_providers

--- lib/providers/store_provider.dart
+++ lib/providers/store_provider.dart
@@ -1,16 +1,17 @@
 import 'package:carpoolz_frontend/services/api_services/store_service.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../services/api_services/socket_service.dart';
 
 class StoreProvider with ChangeNotifier {
-    final userName;
-    String storeName ="";
-    String address = "";
-    LatLng? latLang;
-    String contactNumber = "";
-    String timing = "";
+  final userName;
+  String storeName = "";
+  String address = "";
+  LatLng? latLang;
+  String contactNumber = "";
+  String timing = "";
 
   StoreProvider({
     required this.userName,
@@ -21,15 +22,16 @@ class StoreProvider with ChangeNotifier {
     //  this.timing,
   });
 
+  void setLatLang(LatLng latlang, String address) {
+    this.latLang = latlang;
+    this.address = address;
+  }
+
   Future<void> registerStore(
-    String storeName,
-    String contactNumber,
-    String timing) async {
+      String storeName, String contactNumber, String timing) async {
     try {
-      await StoreService().registerStore(
-        userName.toLowerCase(),
-        storeName, address, latLang!, contactNumber, timing
-      );
+      await StoreService().registerStore(userName.toLowerCase(), storeName,
+          address, latLang!, contactNumber, timing);
 
       notifyListeners();
     } catch (e) {
@@ -37,10 +39,11 @@ class StoreProvider with ChangeNotifier {
     }
   }
 
-  void setLatLang(LatLng latlang, String address ){
-    this.latLang = latlang;
-    this.address = address;
+  Future<Response> getStoreDetails() async {
+    try {
+      return await StoreService().getStoreDetails(userName.toLowerCase());
+    } catch (e) {
+      rethrow;
+    }
   }
 }
-
-
